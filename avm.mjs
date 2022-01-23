bd51abc9181edf4c0f0639d7e8a4c49d6d2f1317
Jason Livesay <runvnc@gmail.com>
2022-01-23 22:31:01 +0000

    update

--- avm.mjs
+++ avm.mjs
@@ -1,51 +1,99 @@
 import algosdk from 'algosdk'
+import delay from 'delay'
+import md5 from 'md5'
 
 let client
-let algod_token, algod_host, algod_port
+let algodtoken, algodhost, algodport
+import fs from 'fs'
 
-export const abiConfig = ({algod_token, algod_host, algod_port} =>{
-  algod_token = token
-  algod_host = host
-  algod_port = port
-})
+export const abiConfig = ({algod_token, algod_host, algod_port}) => {
+  algodtoken = algod_token
+  algodhost = algod_host
+  algodport = algod_port
+}
 
 const getClient = () => {
-  if (!client) client = new algosdk.Algodv2(algod_token, algod_host, algod_port)   	
+  if (!client) client = new algosdk.Algodv2(algodtoken, algodhost, algodport)   	
   
   return client
 }
 
 class ABICaller {
-	constructor(addr, contractJson) {
-    this.addr = addr
-    this.contractJson = contractJson
-    this.acct = new algosdk.Account(addr)
-    this.contract = new algosdk.ABIContract(contractJson)
+	constructor(mnemonic, contract) {
+	  this.acct = algosdk.mnemonicToSecretKey(mnemonic)
+    try {
+      JSON.parse(contract)
+      this.contractData = contract
+    } catch (e) {
+      //try {
+        this.contractData = fs.readFileSync(contract, 'utf8')        
+        this.contractData = JSON.parse(this.contractData)
+      //} catch (e) {
+        
+      //}     
+    }
+
+    this.contract = new algosdk.ABIContract(this.contractData)
     this.client = getClient()
 	}
 
+  dir() {
+    return this.contract.methods.map(m => m.toJSON(m))
+    
+  }
+
   async call(method, args) {
 	  const sp = await this.client.getTransactionParams().do()
-	  
+	  const myid = md5(Date.now() + method + args?.join())
 	  const commonParams = {
 	        appID: this.contract.networks["default"].appID,
 	        sender: this.acct.addr,
 	        suggestedParams:sp,
+	        note: new Uint8Array(Buffer.from(myid)),
 	        signer: algosdk.makeBasicAccountTransactionSigner(this.acct)
 	  }
 
 	  const comp = new algosdk.AtomicTransactionComposer()
-	  const method_ = this.contract.methods.find( m => m.name == funcname )
+	  const method_ = this.contract.methods.find( m => m.name == method )
 	  
 	  comp.addMethodCall({
 	      method: method_, methodArgs: args, ...commonParams
 	  })
-
-	  const txIDs = await this.submit(client)
+    const status = await client.status().do()
+    const lastRound = status['last-round']
+    let currRound = lastRound+1
+	  const txIDs = await comp.submit(client)
 	  const txId = txIDs[0]
-	  const pending = await client.pendingTransactionInformation(txId).do()
-	  
-	  return pending  	
+	  let start = Date.now()
+	  let tries = 0
+	  let found = false
+	  let block
+	  let tx
+	  await delay(1)
+	  while (!found && currRound <= lastRound + 4) {
+	    tries = 0
+  	  while (tries < 28 && !found) {
+    	  try {
+    	    tries += 1
+          block = await client.block(currRound).do()
+          const decoder = new TextDecoder()
+          found = false
+          for (let tx_ of block.block.txns) {
+            let note = decoder.decode(tx_.txn.note)
+            if (note == myid) {
+              found = true
+              tx = tx_
+            }
+          }
+          if (!found) break
+        } catch (e) {
+          await delay(200)       
+        }
+      }
+      currRound++
+    }
+    let elapsed = Date.now() - start
+	  return tx
   }
 }
   
--- test.mjs
+++ test.mjs
@@ -1,5 +1,10 @@
-import {submitAndWatch} from './avm.mjs'
-import {getInterface} from './interface.mjs'
+import {abiConfig, makeCaller} from './avm.mjs'
+import ora from 'ora'
+
+const algod_token = '8854a3be0df4c5495a9e8f62ff7b0b74dc3fe197351bff3d66c4996201a912d0'
+const algod_host = 'https://node.algonfts.art'
+const algod_port = ''
+const mnemonic = 'defy walk load seven paddle clap future normal stuff body glide year blouse south spray range man crawl concert wine comic shrimp boat ability stuff'
 
 const test = async () => {
 
@@ -10,8 +15,21 @@ const test = async () => {
   // sign transactions
 
   // send a transaction to an app
-  // 
+
+  abiConfig({algod_token, algod_host, algod_port})
   
-  submitAndWatch(signed)
-	
+  const service = makeCaller(mnemonic, 'contract.json')
+  
+  //console.log(service.dir())
+  //console.log(service.acct.addr)
+
+  const spinner = ora('Executing ABI call..').start()
+  let res = await service.call('setup',[])
+  spinner.stop()
+  console.log(JSON.stringify(res))
+  console.log('log',res.dt.lg)
+  delete res.dt['lg']
+  console.log('dt',res.dt)
 }
+
+test().catch(console.error)
